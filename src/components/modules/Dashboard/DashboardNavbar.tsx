29db91e8f69ed0dd8c26883ddbd00d51138cc666
sayed725 <ssayed72533@gmail.com>
2026-03-07 15:19:54 +0600

    added full dashboard and navbar for it

--- src/components/modules/Dashboard/DashboardNavbar.tsx
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { getDefaultDashboardRoute } from "@/lib/authUtils"
+import { getNavItemsByRole } from "@/lib/navItems"
+import { getUserInfo } from "@/services/auth.services"
+import { NavSection } from "@/types/dashboard.types"
+import DashboardNavbarContent from "./DashboardNavbarContent"
 
-const DashboardNavbar = () => {
+const DashboardNavbar = async () => {
+   const userInfo = await getUserInfo()
+    const navItems : NavSection[] = getNavItemsByRole(userInfo.role)
+  
+    const dashboardHome = getDefaultDashboardRoute(userInfo.role)
   return (
-    <div>DashboardNavbar</div>
+    <DashboardNavbarContent userInfo={userInfo} navItems={navItems} dashboardHome={dashboardHome}/>
   )
 }
 

71277e077bae1a6a8feb6d39f703e737337e00eb
sayed725 <ssayed72533@gmail.com>
2026-03-07 13:00:28 +0600

    added dashboard side bar and conetent

--- src/components/modules/Dashboard/DashboardSidebar.tsx
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { getDefaultDashboardRoute } from "@/lib/authUtils"
+import { getNavItemsByRole } from "@/lib/navItems"
+import { getUserInfo } from "@/services/auth.services"
+import { NavSection } from "@/types/dashboard.types"
+import DashboardSidebarContent from "./DashboardSidebarContent"
 
-const DashboardSidebar = () => {
+const DashboardSidebar = async () => {
+  const userInfo = await getUserInfo()
+  const navItems : NavSection[] = getNavItemsByRole(userInfo.role)
+
+  const dashboardHome = getDefaultDashboardRoute(userInfo.role)
   return (
-    <div>DashboardSidebar</div>
+    <DashboardSidebarContent userInfo={userInfo} navItems={navItems} dashboardHome={dashboardHome}/>
   )
 }
 
